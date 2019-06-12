ea34732e48e6642b6781b33d11f17589f3c9bf19
radhikasarda <rsa.axelor@gmail.com>
2019-06-12 15:25:23 +0530

    1 Commit

--- src/main/java/demo/HIbernate_ex.java
+++ src/main/java/demo/HIbernate_ex.java
@@ -7,22 +7,52 @@ public class HIbernate_ex {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		 Student student=new Student();
-	    	student.setMarks(80);
-	    	student.setName("Raghav");
-	    	student.setRoll(1);
+         
+		
+		Student_name sname=new Student_name();
+		
+		sname.setFname("Prajjwal");
+		sname.setMname("Kumar");
+		sname.setLname("Singh");
+		
+		
+		
+		
+		//Student s=new Student();
+		Student student= new Student();
+		student.setMarks(95);
+		student.setRoll(1);
+		student.setName(sname);
+		
+		//Student s1,s2;
+		//s1=new Student(2,"Sajal",94);
+		
+		
 	    	
 	    	
-	    	Configuration con=new Configuration().configure();
+	    	Configuration con=new Configuration().configure().addAnnotatedClass(Student.class);
 	    	
 	    	SessionFactory sf=con.buildSessionFactory();
 	    	Session session=sf.openSession();
 	    	session.beginTransaction();
 	    	
+	    	//update
+	    	// Student stuObj = (Student) session.get(Student.class, 2);
+	         //   stuObj.setName("Radhika");
+	         //   stuObj.setMarks(92);
 	    	
-	    	session.save(student);
+	    	//remove
+	    	//Student stu =  (Student) session.get(Student.class,5 );
+	       // session.delete(stu);
+	            
+	            
+	            
+	            session.save(student);
+	    	//s=(Student) session.get(Student.class,1);
+	   // session.save(s1);
+	    	// session.save(s);
 	    	session.getTransaction().commit();
+	    	System.out.println(student);
 	    	
 	}
 
--- src/main/java/demo/Student.java
+++ src/main/java/demo/Student.java
@@ -1,33 +1,59 @@
 package demo;
 
+import javax.annotation.Generated;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
 public class Student {
 	@Id
+	@GeneratedValue
+	@Column
 	private int roll;
-	private String name;
+	@Column
+	private Student_name name;
+	@Column
 	private int marks;
 	
 	
+	Student(){}
+	
+	
+	public Student(int roll, Student_name name, int marks) {
+		super();
+		this.roll = roll;
+		this.name = name;
+		this.marks = marks;
+	}
+
+
 	public int getRoll() {
 		return roll;
 	}
 	public void setRoll(int roll) {
 		this.roll = roll;
 	}
-	public String getName() {
+	
+	public Student_name getName() {
 		return name;
 	}
-	public void setName(String name) {
+
+	public void setName(Student_name name) {
 		this.name = name;
 	}
+
 	public int getMarks() {
 		return marks;
 	}
 	public void setMarks(int marks) {
 		this.marks = marks;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [roll=" + roll + ", name=" + name + ", marks=" + marks + "]";
 	}
 	
 	
